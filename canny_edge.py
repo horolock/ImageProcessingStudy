8914cdacba8f48af82819bab6ddd872bb2d1332a
horolock <horolock96@gmail.com>
2025-02-12 23:41:02 +0900

    thresholds canny edges

--- canny_edge.py
+++ canny_edge.py
@@ -10,19 +10,35 @@ blurred_image = cv2.GaussianBlur(image, (5, 5), 1.4)
 # Canny edge
 canny_edges = cv2.Canny(blurred_image, threshold1=50, threshold2=150)
 
-# Visualize
-fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+# Example 1 Visualize
+# fig, axes = plt.subplots(1, 3, figsize=(15, 5))
 
-axes[0].imshow(image, cmap='gray')
-axes[0].axis('off')
-axes[0].set_title('Original Image')
+# axes[0].imshow(image, cmap='gray')
+# axes[0].axis('off')
+# axes[0].set_title('Original Image')
 
-axes[1].imshow(blurred_image, cmap='gray')
-axes[1].axis('off')
-axes[1].set_title('Gaussian Blurred Image')
+# axes[1].imshow(blurred_image, cmap='gray')
+# axes[1].axis('off')
+# axes[1].set_title('Gaussian Blurred Image')
 
-axes[2].imshow(canny_edges, cmap='gray')
-axes[2].axis('off')
-axes[2].set_title('Canny Edge Detection')
+# axes[2].imshow(canny_edges, cmap='gray')
+# axes[2].axis('off')
+# axes[2].set_title('Canny Edge Detection')
+
+
+# Example 2 Visualize
+thresholds = [(10, 50), (50, 100), (100, 150), (150, 200)]
+canny_results = []
+
+for threshold1, threshold2 in thresholds:
+    canny_image = cv2.Canny(blurred_image, threshold1=threshold1, threshold2=threshold2)
+    canny_results.append(canny_image)
+
+fig, axes = plt.subplots(1, len(thresholds), figsize=(20, 5))
+
+for i, ax in enumerate(axes):
+    ax.imshow(canny_results[i], cmap='gray')
+    ax.axis('off')
+    ax.set_title(f'threshold1={thresholds[i][0]}, threshold2={thresholds[i][1]}')
 
 plt.show()
